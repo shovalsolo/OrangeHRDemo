6e6ebe84a67b4e83c47136e11bb963104e70c045
shovalsolo <shovalso@gmail.com>
2020-02-11 15:05:42 -0500

    Adding all elements from OrangeHRM login screen

--- OrangeHRDemo/src/test/java/pages/LoginPage.java
+++ OrangeHRDemo/src/test/java/pages/LoginPage.java
@@ -23,4 +23,60 @@ public class LoginPage {
 	By logoFacebook = By.xpath("//a[2]//img[1]");							//
 	By logoTweeter = By.xpath("//a[3]//img[1]");							//
 	By logoYoutube = By.xpath("//a[4]//img[1]");							//
+
+	public LoginPage() {													//Constructor that is getting the WebDriver
+		this.driver=driver;													//Initializing the WebDriver
+	}
+	
+	public void loginToOrangeHRM(String uid, String pass) {
+		driver.findElement(username).sendKeys(uid);							//
+		driver.findElement(password).sendKeys(pass);						//
+		driver.findElement(loginButton).click();							//
+	}
+	
+	public void typeUsername(String uid) {
+		driver.findElement(username).sendKeys(uid);
+	}
+	
+	public void typePassword(String pass) {
+		driver.findElement(password).sendKeys(pass);
+	}
+	
+	public void clickOnLoginButton() {
+		driver.findElement(loginButton).click();
+	}
+	
+	public void clickOnforgotPassword() {
+		driver.findElement(forgotPassword).click();
+	}
+	
+	public void clickONRedLoginButton() {
+		driver.findElement(redLoginButton).click();
+	}
+	
+	public void selectFromBottomDropdown(String uid) {
+		driver.findElement(bottomDropdown).sendKeys(uid);
+	}
+	
+	public void clickOnLinkOrangeHRM() {
+		driver.findElement(linkOrangeHRM).click();
+	}
+	
+	public void clickOnlogolinkedin(String uid) {
+		driver.findElement(logolinkedin).click();
+	}
+	
+	public void clickOnlogoFacebook(String uid) {
+		driver.findElement(logoFacebook).click();
+	}
+	
+	public void clickOnlogoTweeter(String uid) {
+		driver.findElement(logoTweeter).click();
+	}
+	
+	public void clickOnlogoYoutube(String uid) {
+		driver.findElement(logoYoutube).click();
+	}
 }
+
+
